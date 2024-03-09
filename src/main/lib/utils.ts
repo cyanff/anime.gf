adad8c411239d68d309234c0258477fddbabcd5f
Phan <thisisphan@gmail.com>
2024-03-09 17:10:45 -0500

    paths working

--- src/main/lib/utils.ts
+++ src/main/lib/utils.ts
@@ -1,10 +1,13 @@
 import { app } from "electron";
-import { join } from "path";
+import { join, dirname } from "path";
 
-export const rootPath = app.getAppPath();
+// App path in production is some/dir/app.asar
+// We strip the app.asar to get the actual path
+export const rootPath = process.env.NODE_ENV === "development" ? app.getAppPath() : dirname(app.getAppPath());
+
 export const dbPath = join(app.getPath("userData"), "agf.db");
-export const unpackedPath = join(rootPath, "resources/resources/app.asar.unpacked");
+
+export const unpackedPath = join(rootPath, "/app.asar.unpacked/resources/");
+
 export const migrationsDir =
-  process.env.NODE_ENV === "development"
-    ? join(process.cwd(), "resources/migrations")
-    : join(unpackedPath, "migrations");
+  process.env.NODE_ENV === "development" ? join(rootPath, "resources/migrations") : join(unpackedPath, "migrations");
