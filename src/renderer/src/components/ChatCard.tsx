1e4d974a129c72e65f400eb1d7be68a6508c3fba
phan <thisisphan@gmail.com>
2024-03-11 03:27:43 -0400

    tailwind class sorter

--- src/renderer/src/components/ChatCard.tsx
+++ src/renderer/src/components/ChatCard.tsx
@@ -14,14 +14,14 @@ export default function ChatCard({ id, name, avatar, msg, className, ...rest }: 
     <div
       {...rest}
       className={cn(
-        "h-[70px] flex items-center space-x-2 w-full p-1 hover:bg-neutral-600 transition ease-out duration-400 rounded-md cursor-pointer",
+        "duration-400 flex h-[70px] w-full cursor-pointer items-center space-x-2 rounded-md p-1 transition ease-out hover:bg-neutral-600",
         className
       )}
     >
       <img className="h-12 w-12 shrink-0 rounded-full object-cover object-top" src={avatar} alt="avatar" />
-      <div className="h-full max-w-full flex flex-col justify-center">
-        <h3 className="text-gray-100 font-normal line-clamp-1">{name}</h3>
-        <p className="text-gray-400 text-[15px] text-sm line-clamp-1">{msg}</p>
+      <div className="flex h-full max-w-full flex-col justify-center">
+        <h3 className="line-clamp-1 font-normal text-gray-100">{name}</h3>
+        <p className="line-clamp-1 text-[15px]  text-gray-400">{msg}</p>
       </div>
     </div>
   );
