7ac22c3c9ba031fe89f59775717333ff13243974
phan <thisisphan@gmail.com>
2024-03-11 02:53:48 -0400

    sidebar

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,9 @@
 import "./styles/global.css";
 import ChatBar from "@/components/ChatBar";
 import { useEffect, useState } from "react";
-import { UIMessage } from "@/lib/types";
 import MessagesArea from "@/components/MessagesArea";
+import ChatCard from "@/components/ChatCard";
+import { Cog8ToothIcon } from "@heroicons/react/24/solid";
 
 function App({ ddbProp }): JSX.Element {
   const [userInput, setUserInput] = useState("");
@@ -17,33 +18,41 @@ function App({ ddbProp }): JSX.Element {
   });
 
   return (
-    <div className="flex h-screen bg-[#373636] p-8 text-sm font-medium text-neutral-200 antialiased lg:text-base">
+    <div className="flex h-screen bg-neutral-800 p-4 text-sm font-medium text-neutral-200 antialiased lg:text-base">
+      {/* Sidebar */}
+      <div className="flex flex-col w-80 h-full bg-neutral-900 rounded-lg mr-8">
+        {/* Chat Cards */}
+        <div className="grow flex flex-col space-y-1 overflow-hidden hover:overflow-y-auto p-4 scrollbar my-3">
+          {ddb.chat_cards.map((card, idx) => {
+            return <ChatCard key={idx} id={card.id} name={card.name} avatar={card.avatar} msg={card.msg} />;
+          })}
+        </div>
+        {/* Utility Bar */}
+        <div className="flex flex-row shrink-0 w-full h-16 bg-neutral-700 p-3 rounded-b-md">
+          <div className="relative">
+            <img src="cyan.png" alt="Avatar" className="h-10 w-10 rounded-full" />
+            <span className="absolute bottom-0 right-0 h-3 w-3 rounded-full ring-4 ring-gray-700 bg-green-400"></span>
+          </div>
+          <div className="h-full flex flex-col justify-center p-2">
+            <h3 className="text-gray-100 font-medium ">cyan</h3>
+            <p className="text-gray-400">Idle</p>
+          </div>
+          <div className="grow flex items-center justify-end">
+            <button className="text-neutral-400 hover:text-neutral-300 ml-5 transition ">
+              <Cog8ToothIcon className="h-6 w-6" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div className="flex h-full w-full flex-row overflow-x-hidden">
         {/* Messages Area and Chat Bar Wrapper*/}
         <div className="flex h-full flex-auto flex-col">
           <div className="flex h-full flex-auto flex-shrink-0 flex-col">
             <MessagesArea msgProp={ddb.chat} />
-            <ChatBar userInput={userInput} setUserInput={setUserInput} typing={true} />
+            <ChatBar userInput={userInput} setUserInput={setUserInput} typing={typing} />
           </div>
         </div>
-      </div>
-      {/* Sidebar */}
-      <div className="ml-8 hidden w-[22rem] flex-shrink-0 flex-col rounded-lg bg-[#222222] px-4 lg:flex">
-        {/* Companion Avatar */}
-        <div className="mt-4 flex w-full flex-col items-center rounded-lg bg-[#3C3C3C] px-4 py-6 shadow-md">
-          <div className="relative h-64 w-64 overflow-hidden rounded-full shadow-md">
-            <img src="saku.png" alt="Avatar" className="h-full w-full object-cover object-top" />
-            <div className="h-10 w-10 rounded-full border-2 border-[#3C3C3C] bg-green-500"></div>
-          </div>
-          <div className="mt-6 w-40 rounded-md bg-[#222222] p-1.5 text-center text-xl font-black text-white">Saku</div>
-        </div>
-        {/* Companion Bio */}
-        <div className="mt-6 flex flex-col p-5">
-          <h3 className="mb-2 text-lg font-bold text-white">About Me</h3>
-          <div className="whitespace-pre-line text-sm leading-6">im saku</div>
-        </div>
-        {/* Utlity Bar */}
-        <div className="relative mt-auto w-full rounded-md p-2" />
       </div>
     </div>
   );
--- src/renderer/src/components/MessagesArea.tsx
+++ src/renderer/src/components/MessagesArea.tsx
@@ -1,41 +1,44 @@
-import { UIMessage } from "@/lib/types";
+import { UIMessage as UIMSG } from "@/lib/types";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Message from "@/components/Message";
 import time from "@/lib/time";
 
 enum ScrollEvent {
-  FETCH_MORE_MESSAGES,
-  NEW_USER_MESSAGE,
-  NEW_COMPANION_MESSAGE
+  FETCH,
+  USER,
+  CHARACTER
 }
 
-interface MessageAreaProps {
-  msgProp: UIMessage[];
+interface Props {
+  msgProp: UIMSG[];
 }
 
-export default function MessagesArea({ msgProp }: MessageAreaProps) {
+export default function MessagesArea({ msgProp }: Props) {
   const [msgs, setMsgs] = useState(msgProp);
-  const scrollHeightRef = useRef(0);
+
+  // ================ Scroll management ================
+  const hRef = useRef(0);
+  const endRef = useRef<HTMLDivElement | null>(null);
   const scrollBoxRef = useRef<HTMLDivElement | null>(null);
   const scrollEventRef = useRef<ScrollEvent | null>(null);
-  const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
-  // This is garbage, will refactor later
   useLayoutEffect(() => {
     const scrollBox = scrollBoxRef.current!;
-    const oldScrollHeight = scrollHeightRef.current;
-    const newScrollHeight = scrollBox.scrollHeight;
+    const oldH = hRef.current;
+    const newH = scrollBox.scrollHeight;
+
     switch (scrollEventRef.current) {
-      case ScrollEvent.FETCH_MORE_MESSAGES:
-        const scrollDelta = newScrollHeight - oldScrollHeight;
+      case ScrollEvent.FETCH:
+        const scrollDelta = newH - oldH;
         scrollBox.scrollTop = scrollDelta;
         break;
-      case ScrollEvent.NEW_COMPANION_MESSAGE:
-        if (oldScrollHeight - scrollBox.scrollTop - scrollBox.clientHeight < 200) {
+      case ScrollEvent.CHARACTER:
+        // If the scroll was near the bottom, scroll to the bottom
+        if (oldH - scrollBox.scrollTop - scrollBox.clientHeight < 200) {
           scrollToEnd();
         }
         break;
-      case ScrollEvent.NEW_USER_MESSAGE:
+      case ScrollEvent.USER:
         scrollToEnd();
         break;
       default:
@@ -48,31 +51,29 @@ export default function MessagesArea({ msgProp }: MessageAreaProps) {
     scrollToEnd();
   }, []);
 
-  // Scroll handler, load more messages when user scrolls to the top
+  function setScrollMarker(height: number, event: ScrollEvent) {
+    hRef.current = height;
+    scrollEventRef.current = event;
+  }
+
+  function scrollToEnd() {
+    const messagesEnd = endRef.current!;
+    messagesEnd.scrollIntoView({ block: "start", behavior: "smooth" });
+  }
+
+  // Load more messages when user scrolls to the top
   function doScroll(e: React.UIEvent<HTMLDivElement, UIEvent>) {
     const scrollBox = e.currentTarget;
-    const firstMessage = scrollBox.firstChild as HTMLElement | null;
-
-    // Get message id to fetch starting from
-
+    const firstMSG = scrollBox.firstChild as HTMLElement | null;
+    const id = firstMSG?.getAttribute("data-msg-id");
     if (scrollBox.scrollTop == 0) {
-      // Store the current scroll height before loading more messages
-      // We use this to restore the scroll position after loading more messages
-      // This is needed to prevent the scroll box from "jumping" to the top of the new messages
-      scrollHeightRef.current = scrollBox.scrollHeight;
-      scrollEventRef.current = ScrollEvent.FETCH_MORE_MESSAGES;
-
+      setScrollMarker(scrollBox.scrollHeight, ScrollEvent.FETCH);
       // Fetch more messages and prepend to the message state
     }
   }
 
-  function scrollToEnd() {
-    const messagesEnd = messagesEndRef.current!;
-    messagesEnd.scrollIntoView({ block: "start", behavior: "smooth" });
-  }
-
   return (
-    <div className="mb-4 flex h-full flex-col overflow-x-auto rounded-lg  bg-[#222222] p-4">
+    <div className="mb-4 flex h-full flex-col overflow-x-auto rounded-lg  p-4">
       <div
         ref={scrollBoxRef}
         onScroll={doScroll}
@@ -92,7 +93,7 @@ export default function MessagesArea({ msgProp }: MessageAreaProps) {
             />
           );
         })}
-        <div ref={messagesEndRef} className="invisible -mt-4 h-0" />
+        <div ref={endRef} className="invisible -mt-4 h-0" />
       </div>
     </div>
   );
